7b81d157a9160fa97e23ffd89ee599eddd3aecf3
ngovanphat99 <phat.ngovan@titancorpvn.com>
2023-10-16 15:49:27 +0700

    initialize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,263 @@
-import Image from 'next/image'
+'use client'
 
+import { useEffect, useRef, useState } from 'react'
+import './chess.scss'
 export default function Home() {
+  const sg = 64 // 64
+  const sb = sg / 16 // 4
+  const ssb = sb / 2 // 2
+  const sqs = [] as any
+  const mks = [
+    [2, 1], [2, 7],
+    [3, 0], [3, 2], [3, 4], [3, 6], [3, 8],
+    [6, 0], [6, 2], [6, 4], [6, 6], [6, 8],
+    [7, 1], [7, 7]
+  ]
+  const chsArr = [
+    [1, '車', 0, 0], [1, '車', 0, 8],
+    [1, '马', 0, 1], [1, '马', 0, 7],
+    [1, '相', 0, 2], [1, '相', 0, 6],
+    [1, '仕', 0, 3], [1, '仕', 0, 5],
+    [1, '帅', 0, 4],
+    [1, '砲', 2, 1], [1, '砲', 2, 7],
+    [1, '兵', 3, 0], [1, '兵', 3, 2], [1, '兵', 3, 4], [1, '兵', 3, 6], [1, '兵', 3, 8],
+    [-1, '車', 9, 0], [-1, '車', 9, 8],
+    [-1, '马', 9, 1], [-1, '马', 9, 7],
+    [-1, '象', 9, 2], [-1, '象', 9, 6],
+    [-1, '士', 9, 3], [-1, '士', 9, 5],
+    [-1, '将', 9, 4],
+    [-1, '炮', 7, 1], [-1, '炮', 7, 7],
+    [-1, '卒', 6, 0], [-1, '卒', 6, 2], [-1, '卒', 6, 4], [-1, '卒', 6, 6], [-1, '卒', 6, 8]
+  ] as any
+
+   /// 玩家走棋
+  let side = 1
+  const done = [-1, -1]
+  const pick = [-1, -1]
+  const abs = Math.abs
+  const round = Math.round
+
+  const bg = useRef<HTMLDivElement>(null);
+  const chess = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if(!bg.current || !chess.current) return
+
+    function placeChess(side: number, name: string, y: number, x: number, i: string){
+      if(!chess.current) return
+      if(chess.current.children.length > 32) return
+      const ch = document.createElement('span') as HTMLSpanElement
+      ch.textContent = name
+      ch.classList.add('ch', side > 0 ? 'red': 'green')
+      ch.setAttribute('i', i)
+      ch.style.top = en(y) + 'px'
+      ch.style.left = en(x) + 'px'
+      chess.current.appendChild(ch)
+    }
+
+    function drawChessBoard() {
+      if(!bg.current) return
+      let rows = document.querySelectorAll('.row')
+      if(rows && rows.length > 0) return;
+      for (let y = 0; y < 9; y++) {
+        const row = document.createElement('div')
+        row.classList.add('row')
+        sqs[y] = row
+        for (let x = 0; x < 8; x++) {
+          const sq = document.createElement('div') as HTMLDivElement
+          sq.classList.add('sq')
+          sqs[y][x] = sq
+          row.appendChild(sq)
+        }
+        bg.current.appendChild(row)
+      }
+      rows = document.querySelectorAll('.row')
+      rows[4].classList.add('middle')
+      sqs[1][4].classList.add('cross')
+      sqs[8][4].classList.add('cross')
+    }
+
+    function drawPosition() {
+      if(!bg.current) return;
+      for (let i = 0; i < mks.length; i++) {
+        const mk = document.createElement('div')
+        mk.classList.add('mk')
+        mk.style.top = (mks[i][0] * sg + ssb) + 'px'
+        mk.style.left = (mks[i][1] * sg + ssb) + 'px'
+        bg.current.appendChild(mk)
+      }
+    }
+
+    const chss = chess.current.querySelectorAll('.ch') as any
+    
+    drawChessBoard()
+    drawPosition();
+    for (let i = 0; i < chsArr.length; i++) {
+      placeChess.apply(null, chsArr[i].concat(i))
+    }
+
+    nextTurn()
+
+
+    document.addEventListener('mousedown', function(e: any){
+      handleAction(e, chss)
+    })   
+  }, [])
+
+
+
+
+  function  handleAction (e: any, chss: any){
+    e = e.originalEvent || e
+    if(!side || !chss || chss.length === 0 || !pick || !bg.current) return
+    if (e.target.classList.contains('ch') &&
+        e.target.classList.contains(side > 0 ? 'red' : 'green')) {
+      if (pick[side] >= 0) {
+        chss[pick[side]].classList.remove('active')
+      }
+      e.target.classList.add('active')
+      pick[side] = +e.target.getAttribute('i')
+      return
+    }
+
+    if (pick[side] >= 0) {
+      let x = de(e.pageX - bg.current.offsetLeft)
+      let y = de(e.pageY - bg.current.offsetTop)
+      if (!(x >= -0.4 && x <= 8.4 &&
+            y >= -0.4 && y <= 9.4)) return
+      if (abs(round(x) - x) > 0.4 ||
+          abs(round(y) - y) > 0.4) return
+      x = round(x)
+      y = round(y)
+      const c = chsArr[pick[side]]
+      if (!canGo(c, x, y)) return
+      chsArr.forEach(function(c: any, i: any){
+        if (!c.dead && c[2] === y && c[3] === x) {
+          c.dead = true
+          chss[i].style.display = 'none'
+        }
+      })
+      const ch = chss[pick[side]]
+      ch.style.left = en(x) + 'px'
+      ch.style.top = en(y) + 'px'
+      c[2] = y
+      c[3] = x
+      if (c[1] === '兵' || c[1] === '卒') {
+        if (side > 0 ? (c[2] >= 5) : (c[2] <= 4)) c.cross = true
+      }
+      done[side] = pick[side]
+      if (done[-side] >= 0) {
+        chss[done[-side]].classList.remove('active')
+      }
+      pick[side] = -1
+      nextTurn()
+      return
+    }
+  }
+
+  function en(n: number){
+    return n * sg + ssb
+  }
+  function de(v: number){
+    return (v - ssb) / sg
+  }
+   
+  function canGo(c: any, x: any, y:any){
+    let dx = x - c[3]
+    let dy = y - c[2]
+    if (c[1] === '兵' || c[1] === '卒') {
+      if (c.cross && dy === 0 && abs(dx) === 1) return true
+      return dx === 0 && dy === c[0]
+    }
+    if (c[1] === '帅' || c[1] === '将') {
+      if (!(
+        c[0] > 0 ? (x >= 3 && x <= 5 && y >= 0 && y <= 2) :
+        (x >= 3 && x <= 5 && y >= 7 && y <= 9)
+      )) return false
+      return abs(dx) + abs(dy) === 1
+    }
+    if (c[1] === '仕' || c[1] === '士') {
+      if (!(
+        c[0] > 0 ? (x >= 3 && x <= 5 && y >= 0 && y <= 2) :
+        (x >= 3 && x <= 5 && y >= 7 && y <= 9)
+      )) return false
+      return abs(dx) * abs(dy) === 1
+    }
+    if (c[1] === '相' || c[1] === '象') {
+      if (!(
+        c[0] > 0 ? (x >= 0 && x <= 8 && y >= 0 && y <= 4) :
+        (x >= 0 && x <= 8 && y >= 5 && y <= 9)
+      )) return false
+      if (chsArr.some(function(c1: any){
+        return !c1.dead &&
+          c1[2] - c[2] === dy / 2 &&
+          c1[3] - c[3] === dx / 2
+      })) return false
+      return abs(dx) === 2 && abs(dy) === 2
+    }
+    if (c[1] === '马') {
+      if (chsArr.some(function(c1: any){
+        return !c1.dead &&
+          c1[2] - c[2] === sign(dy) * (abs(dy)-1) &&
+          c1[3] - c[3] === sign(dx) * (abs(dx)-1)
+      })) return false
+      return abs(dx) * abs(dy) === 2
+    }
+    if (c[1] === '車') {
+      if (dx * dy !== 0) return false
+      const n = chsArr.reduce(function(m: any, c1: any){
+        const dx1 = (c1[3] - c[3]) / sign(dx)
+        const dy1 = (c1[2] - c[2]) / sign(dy)
+        const f = c1 !== c && !c1.dead && (
+          (dy && c1[3] === c[3] && dy1 < abs(dy) && dy1 > 0) ||
+          (dx && c1[2] === c[2] && dx1 < abs(dx) && dx1 > 0)
+        )
+        return f ? m+1 : m
+      }, 0)
+      return n === 0
+    }
+    if (c[1] === '砲' || c[1] === '炮') {
+      if (dx * dy !== 0) return false
+      const n = chsArr.reduce(function(m: any, c1: any){
+        const dx1 = (c1[3] - c[3]) / sign(dx)
+        const dy1 = (c1[2] - c[2]) / sign(dy)
+        const f = c1 !== c && !c1.dead && (
+          (dy && c1[3] === c[3] && dy1 < abs(dy) && dy1 > 0) ||
+          (dx && c1[2] === c[2] && dx1 < abs(dx) && dx1 > 0)
+        )
+        return f ? m+1 : m
+      }, 0)
+      if (chsArr.some(function(c1: any){
+        return !c1.dead && c1[2] === y && c1[3] === x
+      })) {
+        return n === 1
+      }
+      return n === 0
+    }
+  }
+  function sign(v: any){
+    return v > 0 ? 1 :
+    v < 0 ? -1 : 0
+  }
+  function nextTurn(){
+    if (side == null) side = 1
+    else side = -side
+    if (side > 0) {
+      // todo: AI
+      // setTimeout(function(){
+      //   nextTurn()
+      // }, 2000)
+    } else {
+    }
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className="cont-wrap">
+        <div className="cont">
+          <div className="chs" ref={chess!}></div>
+          <div className="bg" ref={bg!}></div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
